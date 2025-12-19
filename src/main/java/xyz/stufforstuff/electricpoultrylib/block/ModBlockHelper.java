bfdbd30a9055b0dce51b59b398012c912b63856a
ElectricPoultryy <doylsamue@protonmail.com>
2025-12-18 22:21:51 -0500

    First Commit

--- src/main/java/xyz/stufforstuff/electricpoultrylib/block/ModBlockHelper.java
+++ src/main/java/xyz/stufforstuff/electricpoultrylib/block/ModBlockHelper.java
@@ -5,9 +5,11 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import xyz.stufforstuff.electricpoultrylib.item.ModItemHelper;
@@ -20,6 +22,18 @@ import static xyz.stufforstuff.electricpoultrylib.ElectricPoultryLib.MODMOD_ID;
 public class ModBlockHelper {
     public static Block createBlock(String blockId, BlockBehaviour.Properties blockProperties, float strength, SoundType soundtype, boolean shouldRegisterItem) {
         return register(blockId, Block::new, blockProperties.strength(strength).sound(soundtype), shouldRegisterItem);
+    }
+
+    public static Block createExperienceDroppingBlock(String blockId, BlockBehaviour.Properties blockProperties, UniformInt uniformInt, float strength, SoundType soundtype, boolean shouldRegisterItem) {
+        ResourceKey<Block> blockKey = keyOfBlock(blockId);
+
+        Block block = new DropExperienceBlock(uniformInt, blockProperties.strength(strength).sound(soundtype).setId(blockKey));
+
+        if (shouldRegisterItem) {
+            ModItemHelper.createBlockItem(blockId, block, new Item.Properties().useBlockDescriptionPrefix());
+        }
+
+        return Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
     }
 
     public static Block createCustomBlock(String blockId, Function<BlockBehaviour.Properties, Block> blockFactory, BlockBehaviour.Properties blockProperties, float strength, SoundType soundtype, boolean shouldRegisterItem) {
@@ -45,10 +59,6 @@ public class ModBlockHelper {
         ResourceKey<Block> blockKey = keyOfBlock(name);
 
         BaseEntityBlock block = blockFactory.apply(settings.setId(blockKey));
-
-        if (shouldRegisterItem) {
-            ModItemHelper.createBlockItem(name, block, new Item.Properties().useBlockDescriptionPrefix());
-        }
 
         return Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
     }
